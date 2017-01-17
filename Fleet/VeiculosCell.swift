016b080847f87953d39edbe9ff6e9853f5897db8
Victor Stillo <victor.stillo@gmail.com>
2017-01-17 12:07:46 -0200

    mudei um arquivo de lugar

---
+++ Fleet/VeiculosCell.swift
@@ -0,0 +1,29 @@
+//
+//  VeiculosCell.swift
+//  Fleet
+//
+//  Created by Student on 1/17/17.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import UIKit
+
+class VeiculosCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+//    override func prepareForReuse() {
+//        <#code#>
+//    }
+    
+
+}
